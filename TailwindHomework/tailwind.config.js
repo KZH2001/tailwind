c32de8e99ead91525e4bc558a6dfe6e5e2c72fb8
KYAWZAWHEIN\Lynx <kyawzawheinoffical2001@gmail.com>
2023-05-22 11:34:24 +0630

    version1

---
+++ TailwindHomework/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        bgColor: 'rgba(248, 240, 240, 0.2)',
+        bgcolor: 'red',
+      },
+    },
+  },
+  plugins: [],
+}
+
